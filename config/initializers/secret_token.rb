ccfab206ee160a713a0b4755dd1197f6d10d8838
Katsuyoshi Ito <kito@itosoft.com>
2011-05-16 22:52:45 +0900

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kitagawa::Application.config.secret_token = '7730605e30bfafa9149dbfe8323f86d31077ec5ec0d73de997804911efcf89876a37b0cff8971ce78a6e982e63370fde2e2e7054693eb09803adfeb80989e617'
