8a17bac9788d80af7fcfb72e9f4155097f4853c8
Katsuyoshi Ito <kito@itosoft.com>
2011-07-18 05:56:25 +0900

    デバック出力削除

--- app/models/yami_rubykaigi_importer.rb
+++ app/models/yami_rubykaigi_importer.rb
@@ -12,7 +12,6 @@ class YamiRubykaigiImporter < Rubykaigi2011Importer
 
   def self.import
     yaml = load_yaml_file_with_key File.join(Rails.root, 'db', self.filename), self.data_file_key
-p yaml
     if yaml
       import_yami_rubykaigi yaml
     end
@@ -27,9 +26,7 @@ private
 
   def self.import_yami_rubykaigi info
     conference = self.conference
-p conference
     parent = conference.events.find_by_code info['parent_id']
-p parent
     info['sub_events'].each do |sub_event_info|
       self.parse_sub_event_of_event parent, 'ja', sub_event_info
       self.parse_sub_event_of_event parent, 'en', sub_event_info
